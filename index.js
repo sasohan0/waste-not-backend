6d4e60b968ab4e399b1ef6e37b61f77bf3caf911
Solih Ahmad Sohan <sasohanme@gmail.com>
2024-06-04 18:12:13 +0600

    port coded

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
